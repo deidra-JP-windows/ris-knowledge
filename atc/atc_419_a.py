fdcc17e68a793dfc4ac31ce6421a0cb44fbffac1
deidra-JP-windows <deidradiscord@gmail.com>
2025-08-16 14:52:27 +0000

    [atc]_419

---
+++ atc/atc_419_a.py
@@ -0,0 +1,9 @@
+s = input()
+if s != "red" and s != "blue" and s != "green":
+    print("Unknown")
+if s == "red":
+    print("SSS")
+if s == "blue":
+    print("FFF")
+if s == "green":
+    print("MMM")
