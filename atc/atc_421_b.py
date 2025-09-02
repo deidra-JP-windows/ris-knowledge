7d3b68c820af92ef3110459f71997cda8861b5ed
deidra-JP-windows <deidradiscord@gmail.com>
2025-09-02 19:12:22 +0000

    [スクリプト修正]_メモ追加

--- atc/atc_421_b.py
+++ atc/atc_421_b.py
@@ -1,26 +1,8 @@
-def solve(n, m, votes):
-    score = [0] * n
-    for vvv in zip(*votes):
-        c0 = vvv.count('0')
-        c1 = n - c0
-        if c0 == 0 or c1 == 0:
-            for i in range(n):
-                score[i] += 1
-            continue
-        if c0 < c1:
-            for i in range(n):
-                if vvv[i] == '0':
-                    score[i] += 1
-        else:
-            for i in range(n):
-                if vvv[i] == '1':
-                    score[i] += 1
-    best = max(score)
-    ans = [i for i, s in enumerate(score, start=1) if s == best]
-    return ans
-
-
-n, m = map(int, input().split())
-votes = [input() for _ in range(n)]
-ans = solve(n, m, votes)
-print(*ans)
+def main():
+    number_list = [0] * 10
+    number_list[0], number_list[1] = map(int, input().split())
+    for i in range(2, 10):
+        number = str(number_list[i - 2] + number_list[i - 1])
+        number_list[i] = int(number[::-1])
+    print(number_list[9])
+main()
